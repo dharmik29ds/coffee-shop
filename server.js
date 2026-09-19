98a58b73cf1a2ce7918228f3d5e934882cc54823
joshi dharmik <jdharmik039@gmail.com>
2026-09-20 00:12:41 +0530

    Secure kitchen dashboard completely

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/order", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "order.html"));
+app.get("/kitchen-secret-99", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
 
 app.get("/admin", (req, res) => {
