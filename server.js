023445c91e4668f38928ec64e987ae1c96b30c55
joshi dharmik <jdharmik039@gmail.com>
2026-09-20 00:39:03 +0530

    Add order route

--- server.js
+++ server.js
@@ -26,13 +26,12 @@ app.use("/api/orders", orderRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("/order", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "order.html"));
+});
 
 app.get("/kitchen-secret-99", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "admin.html"));
-});
-
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
 
 app.listen(PORT, () => {
